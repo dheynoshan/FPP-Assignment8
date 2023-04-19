92056599061eb2fbdd573027e77d7eb84fc2d9c2
dheynoshan <dheynoshan@gmail.com>
2023-04-19 14:35:53 -0500

    Update to the Main.java

--- problem3/Main.java
+++ problem3/Main.java
@@ -1,35 +1,24 @@
 package problem3;
 
 import java.util.ArrayList;
-import java.util.*;
+import java.util.List;
 import java.util.Comparator;
+
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Marketing> l = new ArrayList<>();
+        MarketingArrayList l = new MarketingArrayList();
         l.add(new Marketing("West1", "Prod1", 10000));
         l.add(new Marketing("Rest2", "Prod2", 6000));
         l.add(new Marketing("Pest3", "Prod3", 11000));
         l.add(new Marketing("Qest4", "Prod4", 3000));
         l.add(new Marketing("Fest5", "Prod5", 700));
         l.add(new Marketing("Aest6", "Prod6", 400));
-        System.out.println(sort(l).toString());
+        l.remove(new Marketing("Qest4", "Prod4", 3000));
+        System.out.println(l.getL().toString());
+//        System.out.println(l.sort().toString());
         System.out.println(l.size());
-        System.out.println(listMoreThan1000(l).toString());
-    }
-    public static ArrayList<Marketing> sort(ArrayList<Marketing> aList){
-        class MarketingComparator implements Comparator<Marketing> {
-            @Override
-            public int compare(Marketing m1, Marketing m2){
-                if(m1.getSalesAmount()==m2.getSalesAmount())
-                    return 0;
-                else if (m1.getSalesAmount()>m2.getSalesAmount())
-                    return 1;
-                else
-                    return -1;
-            }
-        }
-        return aList;
+//        System.out.println(listMoreThan1000(l.getL()).toString());
     }
     public static List<Marketing> listMoreThan1000(List<Marketing> list){
         class NameComparator implements Comparator<Marketing>{
